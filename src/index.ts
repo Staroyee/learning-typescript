d36527ffe476a0e6db7b41ba33af95400ff721fc
Staroyee <danmasefield@gmail.com>
2023-11-28 16:54:59 +1030

    Learned about 'array' types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-let sales = 123_456_789;
-let course = 'Typescript';
-let is_published = true;
-let level;
-
-function render(document) {
-    console.log(document);
-}
+let numbers: number[] = [];
+numbers.forEach(n => n.toExponential)
