bead5a3613285ec30ee09f2e0b32ebd3102cf306
Staroyee <danmasefield@gmail.com>
2023-11-28 18:30:04 +1030

    Learned about union types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-type Employee = {
-  readonly id: number;
-  name: string;
-  retire: (date: Date) => void;
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
 }
 
-let employee: Employee = {
-  id: 1,
-  name: "Dan",
-  retire: (date: Date) => {
-    console.log(date);
-  },
-};
+kgToLbs(10);
+kgToLbs("10kg");
